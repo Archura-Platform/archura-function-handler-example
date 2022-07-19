002bce50a2be3b82299c1d1f6db82e9bffefc250
Can Mogol <canmogol@gmail.com>
2022-07-19 16:55:46 +0200

    Add Context and related classes.

--- src/main/java/io/archura/platform/SimpleFunction.java
+++ src/main/java/io/archura/platform/SimpleFunction.java
@@ -1,7 +1,10 @@
 package io.archura.platform;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.archura.platform.cache.Cache;
+import io.archura.platform.context.Context;
 import io.archura.platform.function.Configurable;
+import io.archura.platform.logging.Logger;
 import io.archura.platform.model.Employee;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.HandlerFunction;
@@ -19,72 +22,69 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class SimpleFunction implements HandlerFunction<ServerResponse>, Configurable {
 
-    private final Map<String, Object> configuration = new HashMap<>();
-    private final HttpClient buildInHttpClient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(1000)).build();
+    private Map<String, Object> configuration;
 
     @Override
-    public void setConfiguration(Map<String, Object> config) {
-        this.configuration.putAll(config);
+    public void setConfiguration(Map<String, Object> configuration) {
+        this.configuration = configuration;
     }
 
     @Override
     public ServerResponse handle(ServerRequest request) throws IOException, InterruptedException {
-        log("request = " + request + " configuration: " + configuration);
-        request.attribute(Cache.class.getSimpleName())
-                .map(Cache.class::cast)
-                .ifPresent(cache -> {
-                    try {
-                        final Class<? extends Cache> aClass = cache.getClass();
-                        final Field[] declaredFields = aClass.getDeclaredFields();
-                        log("declaredFields.length = " + declaredFields.length);
-                        for (Field field : declaredFields) {
-                            field.setAccessible(true);
-                            final Object value = field.get(cache);
-                            log("field = " + field + " value: " + value);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+        final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
+        final Optional<Cache> optionalCache = context.getCache();
+        final HttpClient httpClient = context.getHttpClient();
+        final Logger logger = context.getLogger();
+        logger.info("request = " + request + " configuration: " + configuration);
 
-                    final Map<String, Object> cacheData = Map.of(
-                            "k1", "value1",
-                            "k2", 2,
-                            "k3", true,
-                            "k4", new HashMap<>(),
-                            "k5", new Date());
-                    final String keyValues = "keyValues";
-                    cache.put(keyValues, cacheData);
-                    final Map<String, Object> map = cache.get(keyValues);
-                    log("map = " + map);
-                });
+        optionalCache
+                .ifPresent(
+                        cache -> {
+                            try {
+                                final Class<? extends Cache> aClass = cache.getClass();
+                                final Field[] declaredFields = aClass.getDeclaredFields();
+                                logger.info("declaredFields.length = " + declaredFields.length);
+                                for (Field field : declaredFields) {
+                                    field.setAccessible(true);
+                                    final Object value = field.get(cache);
+                                    logger.info("field = " + field + " value: " + value);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            final Map<String, Object> cacheData = Map.of(
+                                    "k1", "value1",
+                                    "k2", 2,
+                                    "k3", true,
+                                    "k4", new HashMap<>(),
+                                    "k5", new Date());
+                            final String keyValues = "keyValues";
+                            cache.put(keyValues, cacheData);
+                            final Map<String, Object> map = cache.get(keyValues);
+                            logger.info("map = " + map);
+                        }
+                );
 
         final HttpRequest httpRequest = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create("http://localhost:9090/sono.json"))
                 .timeout(Duration.ofMillis(100))
                 .build();
-        final HttpResponse<InputStream> httpResponse = buildInHttpClient
+        final HttpResponse<InputStream> httpResponse = httpClient
                 .send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
         final ObjectMapper objectMapper = new ObjectMapper();
         final Employee employee = objectMapper.readValue(httpResponse.body(), Employee.class);
-        log(employee.toString());
+        logger.info(employee.toString());
 
         return ServerResponse.ok()
                 .header("SIMPLE_FUNCTION_HEADER", "GOT_EMPLOYEE")
                 .body(employee);
     }
 
-    private void log(final String log) {
-        String message = String.format("[%s] [%s-%s] [%s]: %s",
-                new Date(),
-                Thread.currentThread().getId(),
-                Thread.currentThread().getName(),
-                this.getClass().getSimpleName(),
-                log);
-        System.out.println(message);
-    }
 }
--- src/main/java/io/archura/platform/exception/FunctionException.java
+++
@@ -1,9 +0,0 @@
-package io.archura.platform.exception;
-
-import java.io.IOException;
-
-public class FunctionException extends RuntimeException {
-    public FunctionException(Exception e) {
-        super(e);
-    }
-}
