7f90bd1ef491468cc629b921188bf0785b096808
Can MOGOL <canmogol@gmail.com>
2022-10-11 22:08:05 +0200

    Remove spring dependency.

--- src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
+++ src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
@@ -1,20 +1,18 @@
 package io.archura.platform.imperativeshell.handler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.archura.platform.api.cache.Cache;
 import io.archura.platform.api.context.Context;
+import io.archura.platform.api.http.HttpServerRequest;
+import io.archura.platform.api.http.HttpServerResponse;
+import io.archura.platform.api.http.HttpStatusCode;
 import io.archura.platform.api.logger.Logger;
 import io.archura.platform.api.stream.LightStream;
 import io.archura.platform.api.type.Configurable;
+import io.archura.platform.api.type.functionalcore.HandlerFunction;
 import io.archura.platform.imperativeshell.handler.model.Employee;
 import io.archura.platform.imperativeshell.handler.model.Movie;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.HandlerFunction;
-import org.springframework.web.servlet.function.ServerRequest;
-import org.springframework.web.servlet.function.ServerResponse;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.net.URI;
@@ -23,14 +21,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 
-@Component
-public class SimpleFunction implements HandlerFunction<ServerResponse>, Configurable {
+public class SimpleFunction implements HandlerFunction<HttpServerResponse>, Configurable {
 
     private Map<String, Object> configuration;
     private final Random random = new Random();
@@ -41,8 +39,8 @@ public class SimpleFunction implements HandlerFunction<ServerResponse>, Configur
     }
 
     @Override
-    public ServerResponse handle(ServerRequest request) throws IOException, InterruptedException {
-        final Context context = (Context) request.attributes().get(Context.class.getSimpleName());
+    public HttpServerResponse handle(HttpServerRequest request) {
+        final Context context = (Context) request.getAttributes().get(Context.class.getSimpleName());
         final Optional<Cache> optionalCache = context.getCache();
         final Optional<LightStream> optionalStream = context.getLightStream();
         final HttpClient httpClient = context.getHttpClient();
@@ -61,46 +59,47 @@ public class SimpleFunction implements HandlerFunction<ServerResponse>, Configur
                                     final Object value = field.get(cache);
                                     logger.info("field = " + field + " value: " + value);
                                 }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
 
-                            final Map<String, Object> cacheData = Map.of(
-                                    "k1", "value1",
-                                    "k2", 2,
-                                    "k3", true,
-                                    "k4", new HashMap<>(),
-                                    "k5", new Date());
-                            final String keyValues = "keyValues";
-                            cache.put(keyValues, cacheData);
-                            final Map<String, Object> map = cache.get(keyValues);
-                            logger.info("map = " + map);
+                                final ObjectMapper objectMapper = context.getObjectMapper();
+                                final Map<String, Object> cacheData = Map.of(
+                                        "k1", "value1",
+                                        "k2", 2,
+                                        "k3", true,
+                                        "k4", new HashMap<>(),
+                                        "k5", new Date());
+                                final String cacheValue = objectMapper.writeValueAsString(cacheData);
+                                final String cacheKey = "keyValues";
+                                logger.info("cache.set, cacheKey: '%s' cacheValue: '%s'", cacheKey, cacheValue);
+                                cache.set(cacheKey, cacheValue);
+                                @SuppressWarnings("unchecked") final Map<String, Object> map = objectMapper.readValue(cache.get(cacheKey), Map.class);
+                                logger.info("cache.get, map: '%s'", map);
+                            } catch (Exception e) {
+                                logger.error("Got error while doing cache operations, error: %s", e.getMessage());
+                            }
                         }
                 );
 
         optionalStream.ifPresent(stream -> {
-            final ObjectMapper objectMapper = context.getObjectMapper();
             final Movie movie = new Movie("Movie Title", random.nextInt(1900, 2000));
-            String movieString = "";
-            try {
-                movieString = objectMapper.writeValueAsString(movie);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            final byte[] value = movieString.getBytes(StandardCharsets.UTF_8);
-
+            final Map<String, String> movieMap = Map.of(
+                    "title", movie.getTitle(),
+                    "year", String.valueOf(movie.getYear())
+            );
             final String topic = Optional.ofNullable(configuration.get("topicName"))
                     .map(String::valueOf)
                     .orElse("movies");
 
-            final String key = stream.send(topic, value).orElse("UNKNOWN_KEY");
-            logger.info("Sent value '%s' for key '%s' to topic '%s'", key, movieString, topic);
+            logger.info("stream.send, topic: '%s', movieMap: '%s', ", topic, movieMap);
+            final String key = stream.send(topic, movieMap);
+            logger.info("Sent value '%s' for key '%s' to topic '%s'", movieMap, key, topic);
         });
 
         final String url = Optional.ofNullable(configuration.get("JSON_URL"))
                 .map(String::valueOf)
                 .orElse("http://localhost:9090/sono.json");
-        final String forwarded = Optional.ofNullable(request.headers().firstHeader("Forwarded"))
+        String forwarded = Optional.ofNullable(request.getRequestHeaders().get("Forwarded"))
+                .orElse(Collections.emptyList())
+                .stream().findFirst()
                 .orElse("");
         final HttpRequest httpRequest = HttpRequest.newBuilder()
                 .GET()
@@ -108,14 +107,25 @@ public class SimpleFunction implements HandlerFunction<ServerResponse>, Configur
                 .header("Forwarded", forwarded)
                 .timeout(Duration.ofMillis(100))
                 .build();
-        final HttpResponse<InputStream> httpResponse = httpClient
-                .send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final Employee employee = objectMapper.readValue(httpResponse.body(), Employee.class);
-        logger.info(employee.toString());
+        try {
+            final HttpResponse<InputStream> httpResponse = httpClient
+                    .send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final InputStream responseInputStream = httpResponse.body();
+            final byte[] bytes = responseInputStream.readAllBytes();
+            final Employee employee = objectMapper.readValue(bytes, Employee.class);
+            logger.info(employee.toString());
 
-        return ServerResponse.ok()
-                .header("SIMPLE_FUNCTION_HEADER", "GOT_EMPLOYEE")
-                .body(employee);
+            return HttpServerResponse.builder()
+                    .header("SIMPLE_FUNCTION_HEADER", "GOT_EMPLOYEE")
+                    .bytes(bytes)
+                    .build();
+        } catch (Exception e) {
+            return HttpServerResponse.builder()
+                    .status(HttpStatusCode.HTTP_INTERNAL_ERROR)
+                    .bytes(String.valueOf(e.getMessage()).getBytes(StandardCharsets.UTF_8))
+                    .build();
+        }
     }
+
 }
