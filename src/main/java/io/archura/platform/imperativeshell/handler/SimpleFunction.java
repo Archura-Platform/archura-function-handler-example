6337483076ae638bcbe8453a4c31c6e85400f5a4
Can MOGOL <canmogol@gmail.com>
2022-10-15 21:14:15 +0200

    Use mapper interface.

--- src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
+++ src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
@@ -1,12 +1,13 @@
 package io.archura.platform.imperativeshell.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.archura.platform.api.cache.Cache;
 import io.archura.platform.api.context.Context;
 import io.archura.platform.api.http.HttpServerRequest;
 import io.archura.platform.api.http.HttpServerResponse;
 import io.archura.platform.api.http.HttpStatusCode;
 import io.archura.platform.api.logger.Logger;
+import io.archura.platform.api.mapper.Mapper;
+import io.archura.platform.api.publish.Publisher;
 import io.archura.platform.api.stream.LightStream;
 import io.archura.platform.api.type.Configurable;
 import io.archura.platform.imperativeshell.handler.model.Employee;
@@ -43,8 +44,10 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
         final Context context = (Context) request.getAttributes().get(Context.class.getSimpleName());
         final Optional<Cache> optionalCache = context.getCache();
         final Optional<LightStream> optionalStream = context.getLightStream();
+        Optional<Publisher> optionalPublisher = context.getPublisher();
         final HttpClient httpClient = context.getHttpClient();
         final Logger logger = context.getLogger();
+        final Mapper mapper = context.getMapper();
         logger.info("request = " + request + " configuration: " + configuration);
 
         optionalCache
@@ -60,18 +63,17 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
                                     logger.info("field = " + field + " value: " + value);
                                 }
 
-                                final ObjectMapper objectMapper = context.getObjectMapper();
                                 final Map<String, Object> cacheData = Map.of(
                                         "k1", "value1",
                                         "k2", 2,
                                         "k3", true,
                                         "k4", new HashMap<>(),
                                         "k5", new Date());
-                                final String cacheValue = objectMapper.writeValueAsString(cacheData);
+                                final String cacheValue = mapper.writeValueAsString(cacheData);
                                 final String cacheKey = "keyValues";
                                 logger.info("cache.set, cacheKey: '%s' cacheValue: '%s'", cacheKey, cacheValue);
                                 cache.set(cacheKey, cacheValue);
-                                @SuppressWarnings("unchecked") final Map<String, Object> map = objectMapper.readValue(cache.get(cacheKey), Map.class);
+                                @SuppressWarnings("unchecked") final Map<String, Object> map = mapper.readValue(cache.get(cacheKey).getBytes(StandardCharsets.UTF_8), Map.class);
                                 logger.info("cache.get, map: '%s'", map);
                             } catch (Exception e) {
                                 logger.error("Got error while doing cache operations, error: %s", e.getMessage());
@@ -139,6 +141,10 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
             logger.info("Sent value '%s' for key '%s' to topic '%s'", movieMap, key, topic);
         });
 
+        optionalPublisher.ifPresent(publisher -> {
+            publisher.publish("message", "Hello! this is an message.");
+        });
+
         final String url = Optional.ofNullable(configuration.get("JSON_URL"))
                 .map(String::valueOf)
                 .orElse("http://localhost:9090/sono.json");
@@ -155,10 +161,9 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
         try {
             final HttpResponse<InputStream> httpResponse = httpClient
                     .send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
-            final ObjectMapper objectMapper = new ObjectMapper();
             final InputStream responseInputStream = httpResponse.body();
             final byte[] bytes = responseInputStream.readAllBytes();
-            final Employee employee = objectMapper.readValue(bytes, Employee.class);
+            final Employee employee = mapper.readValue(bytes, Employee.class);
             logger.info(employee.toString());
 
             return HttpServerResponse.builder()
