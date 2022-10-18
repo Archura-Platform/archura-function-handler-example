adb6e3b32c4feceb9f8a04ac44ac724ea31c62fc
Can MOGOL <canmogol@gmail.com>
2022-10-18 23:17:37 +0200

    Add restriction examples.

--- src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
+++ src/main/java/io/archura/platform/imperativeshell/handler/SimpleFunction.java
@@ -16,8 +16,10 @@ import io.archura.platform.imperativeshell.handler.model.Movie;
 import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
@@ -28,6 +30,9 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.*;
 import java.util.function.Function;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class SimpleFunction implements Function<HttpServerRequest, HttpServerResponse>, Configurable {
 
@@ -48,12 +53,48 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
         final HttpClient httpClient = context.getHttpClient();
         final Logger logger = context.getLogger();
         final Mapper mapper = context.getMapper();
-        logger.info(" configuration: " + configuration);
+        logger.debug("configuration: " + configuration);
 
         try {
-            new Thread(() -> logger.info("NEW THREAD STARTED")).start();
+            new Thread(() -> logger.debug("NEW THREAD STARTED")).start();
         } catch (Exception exception) {
-            logger.error(exception.getMessage());
+            logger.error(getErrorMessage(exception));
+        }
+
+        try {
+            System.exit(1);
+        } catch (Exception exception) {
+            logger.error(getErrorMessage(exception));
+        }
+
+        try {
+            final Process process = Runtime.getRuntime().exec("ls");
+            final OutputStream outputStream = process.getOutputStream();
+            byte[] bytes = new byte[1024];
+            outputStream.write(bytes);
+            logger.debug("Output: %s", new String(bytes));
+        } catch (Exception exception) {
+            logger.error(getErrorMessage(exception));
+        }
+
+        try {
+            final Process process = Runtime.getRuntime().exec(new String[]{"ls"});
+            final OutputStream outputStream = process.getOutputStream();
+            byte[] bytes = new byte[1024];
+            outputStream.write(bytes);
+            logger.debug("Output: %s", new String(bytes));
+        } catch (Exception exception) {
+            logger.error(getErrorMessage(exception));
+        }
+
+        try {
+            final Process process = new ProcessBuilder().command("ls").start();
+            final OutputStream outputStream = process.getOutputStream();
+            byte[] bytes = new byte[1024];
+            outputStream.write(bytes);
+            logger.debug("Output: %s", new String(bytes));
+        } catch (Exception exception) {
+            logger.error(getErrorMessage(exception));
         }
 
         try {
@@ -61,10 +102,10 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
             final Socket s = ss.accept();
             final DataInputStream dis = new DataInputStream(s.getInputStream());
             final String str = dis.readUTF();
-            logger.info("message= " + str);
+            logger.debug("message= " + str);
             ss.close();
         } catch (Exception exception) {
-            logger.error(exception.getMessage());
+            logger.error(getErrorMessage(exception));
         }
 
         try {
@@ -78,33 +119,53 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
             while ((character = reader.read()) != -1) {
                 data.append((char) character);
             }
-            logger.info("data = ", data);
+            logger.debug("data = ", data);
         } catch (Exception exception) {
-            logger.error(exception.getMessage());
+            logger.error(getErrorMessage(exception));
         }
 
+        try {
+            final Class<?> c = Class.forName("io.archura.platform.securitymanager.ArchuraSecurityManager");
+            final Constructor<?> constructor = c.getDeclaredConstructor();
+            final Object o = constructor.newInstance();
+            logger.debug("Created object ArchuraSecurityManager: " + o);
+        } catch (Exception exception) {
+            logger.error(getErrorMessage(exception));
+        }
 
         try {
-            final Class<?> c = Class.forName("io.archura.platform.securitymanager.ThreadSecurityManager");
+            final Class<?> c = Class.forName("io.archura.platform.internal.configuration.GlobalConfiguration");
+            Field[] declaredFields = c.getDeclaredFields();
+            for (Field field : declaredFields) {
+                field.setAccessible(true);
+                Field modifiersField = Field.class.getDeclaredField("modifiers");
+                modifiersField.setAccessible(true);
+                modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+                field.set(null, new Object());
+            }
             final Constructor<?> constructor = c.getDeclaredConstructor();
             constructor.setAccessible(true);
             final Object o = constructor.newInstance();
-            logger.info(">>> io.archura.platform.securitymanager.ThreadSecurityManager: " + o);
+            logger.debug("Created object GlobalConfiguration: " + o);
         } catch (Exception exception) {
-            logger.error(exception.getCause().getMessage());
+            logger.error(getErrorMessage(exception));
         }
 
         optionalCache
                 .ifPresent(
                         cache -> {
                             try {
-                                final Class<? extends Cache> aClass = cache.getClass();
-                                final Field[] declaredFields = aClass.getDeclaredFields();
-                                logger.info("declaredFields.length = " + declaredFields.length);
-                                for (Field field : declaredFields) {
-                                    field.setAccessible(true);
-                                    final Object value = field.get(cache);
-                                    logger.info("field = " + field + " value: " + value);
+                                try {
+                                    final Class<? extends Cache> aClass = cache.getClass();
+                                    final Field[] declaredFields = aClass.getDeclaredFields();
+                                    logger.debug("declaredFields.length = " + declaredFields.length);
+                                    for (Field field : declaredFields) {
+                                        field.setAccessible(true);
+                                        final Object value = field.get(cache);
+                                        logger.debug("field = " + field + " value: " + value);
+                                    }
+                                } catch (Exception exception) {
+                                    logger.error(getErrorMessage(exception));
                                 }
 
                                 final Map<String, Object> cacheData = Map.of(
@@ -115,10 +176,10 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
                                         "k5", new Date());
                                 final String cacheValue = mapper.writeValueAsString(cacheData);
                                 final String cacheKey = "keyValues";
-                                logger.info("cache.set, cacheKey: '%s' cacheValue: '%s'", cacheKey, cacheValue);
+                                logger.debug("cache.set, cacheKey: '%s' cacheValue: '%s'", cacheKey, cacheValue);
                                 cache.set(cacheKey, cacheValue);
                                 @SuppressWarnings("unchecked") final Map<String, Object> map = mapper.readValue(cache.get(cacheKey).getBytes(StandardCharsets.UTF_8), Map.class);
-                                logger.info("cache.get, map: '%s'", map);
+                                logger.debug("cache.get, map: '%s'", map);
                             } catch (Exception e) {
                                 logger.error("Got error while doing cache operations, error: %s", e.getMessage());
                             }
@@ -135,35 +196,37 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
                     .map(String::valueOf)
                     .orElse("movies");
 
-            logger.info("stream.send, topic: '%s', movieMap: '%s', ", topic, movieMap);
+            logger.debug("stream.send, topic: '%s', movieMap: '%s', ", topic, movieMap);
             final String key = stream.send(topic, movieMap);
-            logger.info("Sent value '%s' for key '%s' to topic '%s'", movieMap, key, topic);
+            logger.debug("Sent value '%s' for key '%s' to topic '%s'", movieMap, key, topic);
         });
 
         optionalPublisher.ifPresent(publisher -> {
             publisher.publish("message", "Hello! this is an message.");
         });
 
-        final String url = Optional.ofNullable(configuration.get("JSON_URL"))
-                .map(String::valueOf)
-                .orElse("http://localhost:9090/sono.json");
-        final String forwarded = Optional.ofNullable(request.getRequestHeaders().get("Forwarded"))
-                .orElse(Collections.emptyList())
-                .stream().findFirst()
-                .orElse("");
-        final HttpRequest httpRequest = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url))
-                .header("Forwarded", forwarded)
-                .timeout(Duration.ofMillis(100))
-                .build();
         try {
+            final String url = Optional.ofNullable(configuration.get("JSON_URL"))
+                    .map(String::valueOf)
+                    .orElse("http://localhost:9090/sono.json");
+            logger.debug("Will get JSON from URL: %s", url);
+            final String forwarded = Optional.ofNullable(request.getRequestHeaders().get("Forwarded"))
+                    .orElse(Collections.emptyList())
+                    .stream().findFirst()
+                    .orElse("");
+            final HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(url))
+                    .header("Forwarded", forwarded)
+                    .timeout(Duration.ofMillis(100))
+                    .build();
+
             final HttpResponse<InputStream> httpResponse = httpClient
                     .send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
             final InputStream responseInputStream = httpResponse.body();
             final byte[] bytes = responseInputStream.readAllBytes();
             final Employee employee = mapper.readValue(bytes, Employee.class);
-            logger.info(employee.toString());
+            logger.debug(employee.toString());
 
             return HttpServerResponse.builder()
                     .status(HttpStatusCode.HTTP_OK)
@@ -171,11 +234,31 @@ public class SimpleFunction implements Function<HttpServerRequest, HttpServerRes
                     .bytes(bytes)
                     .build();
         } catch (Exception e) {
+            e.printStackTrace();
+            final String errorMessage = getErrorMessage(e);
+            logger.error("Got error while running function: %s", errorMessage);
             return HttpServerResponse.builder()
                     .status(HttpStatusCode.HTTP_INTERNAL_ERROR)
-                    .bytes(String.valueOf(e.getMessage()).getBytes(StandardCharsets.UTF_8))
+                    .bytes(errorMessage.getBytes(StandardCharsets.UTF_8))
                     .build();
         }
+    }
+
+    private String getErrorMessage(final Exception exception) {
+        if (isNull(exception)) {
+            return "null";
+        }
+        if (nonNull(exception.getMessage())) {
+            return exception.getMessage();
+        }
+        Throwable cause = exception.getCause();
+        while (nonNull(cause)) {
+            if (nonNull(cause.getMessage())) {
+                return cause.getMessage();
+            }
+            cause = cause.getCause();
+        }
+        return "no-error-message-found";
     }
 
 }
